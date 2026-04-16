780c01f91179923c028ff17dcd63df2a59148ca4
paco59140 <pecque18@gmail.com>
2026-04-16 14:47:29 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
-
 android {
     namespace = "com.anthony.reperevocal"
     compileSdk = 35
