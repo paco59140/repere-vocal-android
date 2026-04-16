9c3d5a671afae37a2789be9289d042e94d889a81
paco59140 <pecque18@gmail.com>
2026-04-16 15:31:40 +0200

    Create EmergencyActions.kt

---
+++ app/src/main/java/com/anthony/reperevocal/services/EmergencyActions.kt
@@ -0,0 +1,8 @@
+package com.anthony.reperevocal.services
+
+import android.content.Context
+
+object EmergencyActions {
+    fun callTrustedContact(context: Context, phone: String) {}
+    fun sendEmergencySms(context: Context, phone: String, message: String) {}
+}
